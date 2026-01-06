8bd41128dd8f67e9f822dea81f2566cd0ba124b6
Rangga Maulana <me@ranggamaulana.xyz>
2026-01-06 12:55:16 +0800

    feat: Refactor bank statement models into separate files and enhance views with new actions and a button to open statement lines.

--- xyz_account/models/__init__.py
+++ xyz_account/models/__init__.py
@@ -1,1 +1,2 @@
-from . import bank_statement
+from . import account_bank_statement
+from . import account_bank_statement_line
--- xyz_account/models/bank_statement.py
+++
@@ -1,5 +0,0 @@
-from odoo import models
-
-class BankStatement(models.Model):
- _name = 'account.bank.statement'
- _inherit = ['account.bank.statement', 'mail.thread', 'mail.activity.mixin']
